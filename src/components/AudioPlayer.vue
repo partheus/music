6738f2b56b4fe7b4aa8a4932c3e226b6227f7e56
Parth <parth.shiralkar@gmail.com>
2024-02-18 16:08:16 -0800

    Create basic audio player

---
+++ src/components/AudioPlayer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="audio-player">
+    <audio controls>
+      <source :src="audioSrc" type="audio/wav">
+      Your browser does not support the audio element.
+    </audio>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AudioPlayer',
+  data() {
+    return {
+      audioSrc: require('@/assets/synthpop.wav')
+    };
+  }
+};
+</script>
+
+<style scoped>
+.audio-player {
+  /* Styling for your audio player */
+}
+</style>
