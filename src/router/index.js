efefb025dba631249e149ab31bebd856bd59bd27
Parth <parth.shiralkar@gmail.com>
2024-02-19 01:25:05 -0800

    Add notfound page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 // /src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
+import NotFound from "@/views/NotFound.vue";
 // Import other pages you create
 
 const routes = [
@@ -8,16 +9,38 @@ const routes = [
         path: '/',
         name: 'Home',
         component: HomePage,
+        meta: {
+            title: 'hTrap Music',
+        },
     },
     {
         path: '/originals',
         name: 'Originals',
         component: () => import('../views/OriginalMusic.vue'),
+        meta: {
+            title: 'Original Music | hTrap',
+        },
     },
     {
         path: '/remixes',
         name: 'Remixes',
         component: () => import('../views/RemixMusic.vue'),
+        meta: {
+            title: 'Remix Music | hTrap',
+        },
+    },
+    {
+        path: '/colophon',
+        name: 'Colophon',
+        component: () => import('../views/ColophonPage.vue'),
+        meta: {
+            title: 'Colophon | hTrap',
+        },
+    },
+    {
+        path: '/:catchAll(.*)*', // This will match all routes that are not matched by other route records
+        name: 'NotFound',
+        component: NotFound,
     }
     // Add other route objects here
 ];
@@ -25,6 +48,14 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes,
+});
+
+router.afterEach((to) => {
+    // Use next tick to handle router history correctly
+    let Vue = require('vue');
+    Vue.nextTick(() => {
+        document.title = to.meta.title || 'hTrap Music'; // Set the page title using the route meta
+    }).then(r => r);
 });
 
 export default router;
---
+++ src/views/NotFound.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="not-found">
+    <h1>404</h1>
+    <img class="not-found-image" src="@/assets/notfound.png" alt="Not Found"/>
+    <router-link to="/">Go Back Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+};
+</script>
+
+<style>
+/* Style as needed */
+.not-found {
+  text-align: center;
+}
+
+.not-found-image {
+  display: block;
+  margin: auto;
+  width: auto;
+  max-height: 40vh;
+}
+
+.not-found h1 {
+  font-size: 4em;
+  margin: 0;
+}
+
+.not-found p {
+  font-size: 2em;
+}
+</style>
