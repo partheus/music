bf2d2277df21625a5530823137b310f81a99232b
Parth <parth.shiralkar@gmail.com>
2024-02-18 17:09:14 -0800

    Create HomePage view

---
+++ src/views/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Welcome to My Music Interaction Website</h1>
+    <audio-player></audio-player>
+    <!-- Other content for Home page -->
+  </div>
+</template>
+
+<script>
+import AudioPlayer from '../components/AudioPlayer.vue';
+
+export default {
+  name: 'HomePage', // Updated the name of the component
+  components: {
+    AudioPlayer
+  }
+};
+</script>
