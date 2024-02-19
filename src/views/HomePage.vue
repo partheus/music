c18527bceeec7ff60ca763c53be084632604e5fd
Parth <parth.shiralkar@gmail.com>
2024-02-18 21:47:29 -0800

    Fix homepage styling and nav

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <h1>musique</h1>
+    <h1>musique!</h1>
     <p>Navigate to the pages and play some music</p>
+    <br>
+    <small>Volume control is available on all pages</small>
+    <br>
+    <small>[a project by Parth Shiralkar]</small>
     <!-- Other content for Home page -->
   </div>
 </template>
