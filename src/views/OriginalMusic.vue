63a2b7f0d222f7457ba15afae02ec07322618e3a
Parth <parth.shiralkar@gmail.com>
2024-02-18 21:47:43 -0800

    Fix tracklist styles

--- src/views/OriginalMusic.vue
+++ src/views/OriginalMusic.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="original-music">
     <h2>Original Music</h2>
-    <ul>
+    <ul class="track-list">
       <li v-for="track in tracks" :key="track.id" @click="selectTrack(track)" class="track-item">
         <div class="track-info">
           <img :src="track.cover" :alt="`${track.title} cover`" class="track-cover">
@@ -38,26 +38,35 @@ export default {
 </script>
 
 <style scoped>
-.original-music {
-  padding: 1rem;
-}
-ul {
+.track-list {
   list-style: none;
-  padding: 0;
+  padding: 1em;
+  margin: 20px auto; /* Adjust top and bottom margins as needed */
+  height: 40vh; /* Adjust based on the desired fraction of the viewport height */
+  overflow-y: auto; /* Show scrollbar only when necessary */
+
+  @media (min-width: 768px) {
+    max-width: 60vw; /* Adjust based on the desired width */
+  }
 }
 .track-item {
   display: flex;
   align-items: center;
-  margin-bottom: 1rem;
+  margin-bottom: 1em;
   cursor: pointer;
-  transition: background-color 0.3s ease;
+  transition: all 0.3s ease;
+  background: #f9f9f9;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.124);
+  padding: 1em 2em;
+  border-radius: 0.5em;
+  font-size: 70%;
 }
 .track-item:hover {
-  background-color: #f0f0f0;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.32);
 }
 .track-cover {
-  width: 50px;
-  height: 50px;
+  width: 5em;
+  height: 5em;
   object-fit: cover;
   margin-right: 1rem;
   border-radius: 50%;
@@ -69,10 +78,17 @@ ul {
 .track-details h3 {
   margin: 0;
   color: #333;
-  font-size: 1.2rem;
+  font-weight: bold;
+
+  @media (min-width: 768px) {
+    font-size: 1.5rem;
+  }
 }
 .track-details p {
   margin: 0;
   color: #666;
-  font-size: 1rem;
+  font-size: 1.25rem;
+}
+.track-details{
+  text-align: left;
 }</style>
