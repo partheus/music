ee5409d8eb9527869d3062af54411b3a8f5cb446
Parth <parth.shiralkar@gmail.com>
2024-02-19 01:25:55 -0800

    Create colophon

---
+++ src/views/ColophonPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="colophon">
+    <h1>Colophon</h1>
+    <h4>An inscription placed usually at the end of a book, giving facts about its publication.</h4>
+    <ul>
+      <li>Created by Parth Shiralkar</li>
+      <li>Powered by Vue.js</li>
+      <li>Hosted on Netlify</li>
+      <li>Source code available on <a href="https://github.com" target="_blank">GitHub</a></li>
+    </ul>
+  </div>
+</template>
