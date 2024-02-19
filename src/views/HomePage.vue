bda4b4072f894047f2efa2a6f400dc9f7c3f731c
Parth <parth.shiralkar@gmail.com>
2024-02-18 17:31:25 -0800

    Clean homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,18 +1,13 @@
 <template>
   <div>
-    <h1>Welcome to My Music Interaction Website</h1>
+    <h1>hehehehehe</h1>
     <audio-player></audio-player>
     <!-- Other content for Home page -->
   </div>
 </template>
 
 <script>
-import AudioPlayer from '../components/AudioPlayer.vue';
-
 export default {
   name: 'HomePage', // Updated the name of the component
-  components: {
-    AudioPlayer
-  }
 };
 </script>
