ae0c820c6d6c7d63af328bbb872b578d56df145b
Parth <parth.shiralkar@gmail.com>
2024-02-19 21:34:09 -0800

    Add colophon to nav, update colophon

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <div>
+  <header>
+    <!-- Nav -->
     <nav>
       <ul>
         <li><router-link to="/">Home</router-link></li>
         <li><router-link to="/originals">Originals</router-link></li>
         <li><router-link to="/remixes">Remixes</router-link></li>
+        <li><router-link to="/colophon">Colophon</router-link></li>
         <!-- Add more navigation links as needed -->
       </ul>
     </nav>
-  </div>
-
-  <header>
     <!-- Volume Control Slider -->
     <div class="volume-control">
-      <label for="volume-slider">Volume:</label>
+      <label for="volume-slider">site volume:</label>
       <input type="range" width="200px" id="volume-slider" min="0" max="1" step="0.01"
              v-model="volume" @input="adjustVolume" />
     </div>
   </header>
 
-  <div id="app">
-    <router-view/><!--    <audio-player></audio-player>-->
+  <div>
+    <router-view/>
     <persistent-player :volume="volume" />
   </div>
 
--- src/views/ColophonPage.vue
+++ src/views/ColophonPage.vue
@@ -5,8 +5,12 @@
     <ul>
       <li>Created by Parth Shiralkar</li>
       <li>Powered by Vue.js</li>
-      <li>Hosted on Netlify</li>
+      <li>Hosted on Netlify and CloudFlare</li>
+      <li>Music by Parth Shiralkar</li>
+      <li>Test <router-link to="/not-a-link">404 Error Page</router-link></li>
       <li>Source code available on <a href="https://github.com" target="_blank">GitHub</a></li>
     </ul>
   </div>
 </template>
+<script setup>
+</script>
