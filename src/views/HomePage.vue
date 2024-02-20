b16b445e58219094a963bfb6c3cb55acafa81fdb
Parth <parth.shiralkar@gmail.com>
2024-02-19 21:34:43 -0800

    Fix frontmatter

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,9 +3,9 @@
     <h1>musique!</h1>
     <p>Navigate to the pages and play some music</p>
     <br>
-    <small>Volume control is available on all pages.</small>
+    <small>Volume control is available on all pages. This site is under construction.</small>
     <br>
-    <router-link to="/colophon">[a project by Parth Shiralkar]</router-link>
+    [a project by Parth Shiralkar]
     <!-- Other content for Home page -->
   </div>
 </template>
