cddeaacf70b86bc3dde27a17985afa4aa3c0cf04
Parth <parth.shiralkar@gmail.com>
2024-02-27 13:02:02 -0800

    Update frontmatter, add redirect to main site

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,6 +6,8 @@
     <small>Volume control is available on all pages. This site is under construction.</small>
     <br>
     [a project by Parth Shiralkar]
+    <br>
+    visit <a href="https://parth.ninja" title="Main site of Parth Shiralkar" target="_blank">main site</a>
     <!-- Other content for Home page -->
   </div>
 </template>
