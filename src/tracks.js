d37ebc4d8bd978b43f4e41a10c435542085b90a2
Parth <parth.shiralkar@gmail.com>
2024-02-29 00:57:15 -0800

    Update tracklist

--- src/tracks.js
+++ src/tracks.js
@@ -11,7 +11,7 @@ const trackData = [
     },
     {
         id: 2,
-        title: 'funkjam.wav',
+        title: 'frantic antics',
         artist: 'htrap',
         filename: 'funkjam.wav',
         cover: require('@/assets/album-art/9e8e9e.png'),
@@ -19,7 +19,7 @@ const trackData = [
     },
     {
         id: 3,
-        title: 'jazz.wav',
+        title: 'all over again (demo loop)',
         artist: 'htrap',
         filename: 'bday.wav',
         cover: require('@/assets/album-art/520b22.png'),
@@ -27,7 +27,7 @@ const trackData = [
     },
     {
         id: 4,
-        title: 'synthpop.wav',
+        title: 'beach business',
         artist: 'htrap',
         filename: 'synthpop.wav',
         cover: require('@/assets/album-art/520b51.png'),
@@ -35,7 +35,7 @@ const trackData = [
     },
     {
         id: 5,
-        title: 'janglebop.wav',
+        title: 'the role of dice',
         artist: 'htrap',
         filename: 'janglebop.wav',
         cover: require('@/assets/album-art/f095f0.png'),
@@ -43,7 +43,7 @@ const trackData = [
     },
     {
         id: 6,
-        title: 'wavy.wav',
+        title: 'acoustic bass for lunch',
         artist: 'htrap',
         filename: 'wavyexport.wav',
         cover: require('@/assets/album-art/ffffff.png'),
@@ -51,7 +51,7 @@ const trackData = [
     },
     {
         id: 7,
-        title: 'chronic.wav (asthma - playboi carti remix)',
+        title: 'chronic.wav ("asthma" - playboi carti remix)',
         artist: 'htrap',
         filename: 'chronic.wav',
         cover: require('@/assets/album-art/ffffff.png'),
@@ -59,7 +59,7 @@ const trackData = [
     },
     {
         id: 8,
-        title: 'old ports [teen x - playboi carti remix (carti verse only)]',
+        title: 'old ports ["teen x" - playboi carti remix (carti verse only)]',
         artist: 'htrap',
         filename: 'ports.mp3',
         cover: require('@/assets/album-art/ffffff.png'),
@@ -67,7 +67,7 @@ const trackData = [
     },
     {
         id: 9,
-        title: 'mountain dew (lil dicky dave season 2 lofi hiphop remix demo)',
+        title: 'mountain dew (dave season 2 - lil dicky lofi hiphop remix demo)',
         artist: 'htrap',
         filename: 'mountainDew.mp3',
         cover: require('@/assets/album-art/ffffff.png'),
@@ -91,7 +91,7 @@ const trackData = [
     },
     {
         id: 12,
-        title: 'chose [bikini bottom - ice spice (verse one remix)]',
+        title: 'chose ["bikini bottom" - ice spice (verse one remix)]',
         artist: 'htrap',
         filename: 'chose.mp3',
         cover: require('@/assets/album-art/ffffff.png'),
@@ -107,7 +107,7 @@ const trackData = [
     },
     {
         id: 14,
-        title: 'red sprite (playboi carti "cancun" remake)',
+        title: 'red sprite ("cancun" - playboi carti remake)',
         artist: 'htrap',
         filename: 'sprite.mp3',
         cover: require('@/assets/album-art/ffffff.png'),
@@ -123,11 +123,35 @@ const trackData = [
     },
     {
         id: 16,
-        title: 'nautilus (what\'s next by drake remade)',
+        title: 'nautilus ("what\'s next" - drake remake)',
         artist: 'htrap',
         filename: 'nautilus.mp3',
         cover: require('@/assets/album-art/ffffff.png'),
         type: 'remix'
+    },
+    {
+        id: 17,
+        title: 'retale value',
+        artist: 'htrap',
+        filename: 'retale.mp3',
+        cover: require('@/assets/album-art/ffffff.png'),
+        type: 'original'
+    },
+    {
+        id: 18,
+        title: 'freefall ("falling down" - @GadManDubs remix)',
+        artist: 'htrap',
+        filename: 'freefall.mp3',
+        cover: require('@/assets/album-art/ffffff.png'),
+        type: 'remix'
+    },
+    {
+        id: 19,
+        title: 'grocery',
+        artist: 'htrap',
+        filename: 'grocery.mp3',
+        cover: require('@/assets/album-art/ffffff.png'),
+        type: 'original'
     }
 
 
