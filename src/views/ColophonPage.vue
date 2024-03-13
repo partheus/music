c3b0cb4a9857c90b9044c658eb5780e5c05af7ba
Parth <parth.shiralkar@gmail.com>
2024-03-13 01:36:08 -0700

    Update colophon markup, content

--- src/views/ColophonPage.vue
+++ src/views/ColophonPage.vue
@@ -3,12 +3,23 @@
     <h1>Colophon</h1>
     <h4>An inscription placed usually at the end of a book, giving facts about its publication.</h4>
     <ul>
-      <li>Created by Parth Shiralkar</li>
-      <li>Powered by Vue.js</li>
-      <li>Hosted on Netlify and CloudFlare</li>
-      <li>Music by Parth Shiralkar</li>
-      <li>Test <router-link to="/not-a-link">404 Error Page</router-link></li>
-      <li>Source code available on <a href="https://github.com" target="_blank">GitHub</a></li>
+      <li>Site designed and developed by Parth Shiralkar
+        <blockquote>~ with HTML, CSS, Love, Death, Robots, the Extraordinary Art of Napping Soundly</blockquote>
+      </li>
+      <li>Powered by Vue.js
+        <blockquote>~ with Vue Router and Vuex</blockquote>
+      </li>
+      <li>Hosted on Netlify
+        <blockquote>~ with continuous deployment from GitHub</blockquote>
+      </li>
+      <li>All music created by Parth Shiralkar&copy;
+        <blockquote>~ where used, samples and vocals are specified in the track details</blockquote></li>
+      <li>Test <router-link to="/not-a-link">404 Error Page</router-link>
+        <blockquote>~ because why not?</blockquote>
+      </li>
+      <li>\(^_^)/
+        <blockquote>~ <a href="https://parth.ninja" title="Main site of Parth Shiralkar" target="_blank">Parth Shiralkar</a></blockquote>
+      </li>
     </ul>
   </div>
 </template>
