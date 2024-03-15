6878ab5a2bcbbff6263479487afaf81de88f1bf2
Parth <parth.shiralkar@gmail.com>
2024-03-15 01:27:20 -0700

    Create about page

---
+++ src/views/AboutPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <h4>A little bit about this site</h4>
+
+    <p>This site is a project by Parth Shiralkar, also known as hTrap. It is a playground for me to experiment with
+      Vue.js and other web technologies. The site is under construction and will be updated in several time units.</p>
+
+    <p>It is also a place where I can showcase my music. All music on this site is created by me using a combination of
+      an old Akai MIDI controller and Fruity Loops. I am not a professional musician, and I do not make a living from my
+      music. I make music because it's a thoroughly fun activity.</p>
+
+    <p>Feel free to navigate to the
+      <router-link to="/originals">Original Music</router-link>
+      and
+      <router-link to="/remixes">Remixes</router-link>
+      pages to listen to some of my music. Volume control is available on all pages.
+    </p>
+
+    <p>
+      If you would like to know more about the site, visit the
+      <router-link to="/colophon">Colophon</router-link> page.
+    </p>
+
+    <p>For more information about me, visit my main site at <a href="https://parth.ninja"
+                                                               title="Main site of Parth Shiralkar" target="_blank">parth.ninja</a>.
+    </p>
+
+    <p>Thank you for visiting!</p>
+
+  </div>
+</template>
