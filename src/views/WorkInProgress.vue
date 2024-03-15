f0756a59142970ead1487c84878fd067448c3309
Parth <parth.shiralkar@gmail.com>
2024-03-15 14:43:20 -0700

    Add wip section

--- src/views/WorkInProgress.vue
+++ src/views/WorkInProgress.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
     <h1>Work in Progress</h1>
-    <h4>Under construction</h4>
+    <h4>Stuff that is mostly under construction</h4>
+
+    <div class="wip-project">
+      <p>A simple drum and bass remix sampling the famous speech by Carl Sagan, "Pale Blue Dot".</p>
+      <video width="320" height="auto" controls>
+        <source src="@/assets/paleBlueDot.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+
+    <div class="wip-project">
+      <p>
+        A simple pluggnb beat with Pooh Bear dancing to it.
+      </p>
+      <video width="320" height="auto" controls>
+        <source src="@/assets/pooh.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+
+    <div class="wip-project">
+      <p>
+        A simple beat with Monkey D. Luffy beating up Crocodile.
+      </p>
+      <video width="320" height="auto" controls>
+        <source src="@/assets/luffy.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
   </div>
 </template>
+<script setup>
+</script>
